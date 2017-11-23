bae87571dea4083bbfa2c8d226d7835f6b644e9c
Christopher Wolff <cfw13@duke.edu>
2017-11-23 15:01:30 -0500

    Initial commit

---
+++ MultiplesOf3And5.java
@@ -0,0 +1,23 @@
+/**
+ * https://projecteuler.net/problem=1
+ */
+public class MultiplesOf3And5 {
+
+    /**
+     * Calculates the numbers below n that are mutiples of 3 or 5
+     */
+    public static int solve(int n) {
+        int count = 0;
+        for (int i = 1; i < n; i++) {
+            if (i % 3 == 0 || i % 5 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(solve(1000));
+    }
+
+}
