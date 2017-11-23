7ced042c7778a59bf0ef8be3ef7ba207f7050f29
Christopher Wolff <cfw13@duke.edu>
2017-11-23 15:41:35 -0500

    Solve problem

---
+++ Problem3.java
@@ -0,0 +1,27 @@
+/**
+ * Problem 3: Largest prime factor
+ *
+ * https://projecteuler.net/problem=3
+ */
+ public class Problem3 {
+
+     /**
+      * Returns the largest prime factor of n
+      */
+     public static long solve(long n) {
+         long largest = 1L;
+         for (long i = 2L; i <= n; i++) {
+             if (n % i == 0) {
+                 largest = Math.max(largest, i);
+                 n /= i;
+             }
+         }
+         return largest;
+    }
+
+     public static void main(String[] args) {
+         long input = (args.length > 0) ? Integer.valueOf(args[0]) : 600851475143L;
+         System.out.println(solve(input));
+     }
+
+ }
